f7363dc95a92e65d1d57a472d0008c19c33d4c91
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-20 17:36:30 -0500

    saving user data and creating login and register post requests

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,11 +6,13 @@ import db from '../db.js'
 const router = express.Router()
 
 router.post('/register', (req, res) => {
-
+    const {username, password} = req.body
+    console.log(username, password)
+    res.sendStatus(201)
 })
 
 router.post('/login', (req, res) => {
-    
+
 })
 
 
