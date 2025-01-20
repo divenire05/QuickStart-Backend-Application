1e9b9f954f8d844533918cbd34accf03c09388f7
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-19 19:33:25 -0500

    directing requests and middleware

--- src/server.js
+++ src/server.js
@@ -1,7 +1,22 @@
 import express from 'express'
+import path, {dirname} from 'path'
+import {fileURLToPath} from 'url'
 
 const app = express()
 const PORT = process.env.PORT || 5003
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+
+// Middleware
+app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.json())
+
+
+app.get('/', (req, res) => { // INCORRECT BUT ABOVE LINE SAVES
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 app.listen(PORT, () => {
     console.log(`Server has started on port: ${PORT}`)
