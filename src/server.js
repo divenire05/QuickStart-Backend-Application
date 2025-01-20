9f9ef801e7ce9ee0f03465795e40d50c125001cc
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-20 04:17:01 -0500

    adding todo routes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import path, {dirname} from 'path'
 import {fileURLToPath} from 'url'
 import authRoutes from './routes/authRoutes.js'
+import todoRoutes from '/routes/todoRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT || 5003
@@ -22,6 +23,7 @@ app.get('/', (req, res) => { // INCORRECT BUT ABOVE LINE SAVES
 
 // Routes
 app.use('/auth', authRoutes)
+app.use('/todos', todoRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server has started on port: ${PORT}`)
