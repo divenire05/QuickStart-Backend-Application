b7c02dc1d4df42a9b5513c937f54fc5c3d7fac82
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-19 20:20:27 -0500

    configuring authRoutes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import path, {dirname} from 'path'
 import {fileURLToPath} from 'url'
+import authRoutes from './routes/authRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT || 5003
@@ -17,6 +18,10 @@ app.use(express.json())
 app.get('/', (req, res) => { // INCORRECT BUT ABOVE LINE SAVES
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
+
+
+// Routes
+app.use('/auth', authRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server has started on port: ${PORT}`)
