4dfa77e946a3712d88e0872c78b547852c95d654
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-21 03:12:03 -0500

    adding registration route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,7 +7,25 @@ const router = express.Router()
 
 router.post('/register', (req, res) => {
     const {username, password} = req.body
-    console.log(username, password)
+
+    const hashedPassword = bcrypt.hashSync(password, 8)
+    console.log(hashedPassword)
+
+    try {
+        const insertUser = db.prepare(`INSERT INTO users (username, password) VALUES (?, ?)`)
+        const result = insertUser.run(username, hashedPassword)
+
+        const defaultTodo = `Hello :) Add your first todo!`
+        const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`) // WHY THE QUESTION MARK?
+        insertTodo.run(result.lastInsertRowid, defaultTodo)
+
+        const token = jwt.sign({id: result.lastInsertRowid}, process.env.JWT_SECRET, {expiresIn: '24h'})
+        res.json({token})
+    } catch (err) {
+        console.log(err.message)
+        res.sendStatus(503)
+    }
+
     res.sendStatus(201)
 })
 
