a8c1c6fc696d58c84034e5c07148f889676f650b
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-29 21:37:42 -0500

    handling the put endpoint

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -21,7 +21,13 @@ router.post('/', (req, res) => {
 
 // Update a todo
 router.put('/:id', (req, res) => {
+    const {completed} = req.body
+    const {id} = req.params
 
+    const updatedTodo = db.prepare(`UPDATE todos SET completed = ? WHERE id = ?`)
+    updatedTodo.run(completed, id)
+
+    res.json({message: "Todo completed"})
 })
 
 // Delete a todo
